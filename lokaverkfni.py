0b27b5beb85bcdb363a1bd0ef0a273fe7b8daaab
sigurdurBl <sigurdureg210@nemi.tskoli.is>
2017-11-24 10:01:19 +0000

    gerðum leið til að lesa um úr skrá bill

--- lokaverkfni.py
+++ lokaverkfni.py
@@ -27,14 +27,46 @@ class bill:
         self.ath = ath
 
 
+
 class pantanair:
     def __init__(self,audkenni,vidskiptavinir,pontunardagur,ath):
         self.audkenni = audkenni
         self.vidskiptavinir = vidskiptavinir
         self.pontunardagur = pontunardagur
         self.ath = ath
+def readFile(skra):
+    bilar = []
+    with open(skra,"r") as bill1:
+        lina=bill1.read()
+
+        for line in bill1:
+            #line.r
+            bilar.append(line)
+
+    return bilar
 
 
 
+val = input("hvað ertu að leita af")
+#val = "\"" + val + "\""
+bilar = []
+lina=""
+with open("bill.txt","r") as bill1:
+    lina=bill1.read()
+    bilar=lina.split(":")
+    bilar.remove("")
+    #bilar=lina.split(",")
+print(bilar)
+temp=[]
+for x in bilar:
+    temp = x.split(",")
 
-
+    print(val)
+    #print(temp[2])
+    temp[2]=temp[2][1:-1]
+    print("verður svona",temp[2])
+    if val==temp[2]:
+        print(temp)
+    #print(temp[2])
+    #for lin in bilar:
+       #print(lin)
